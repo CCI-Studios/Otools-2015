d48210b52888bc0b19683d6bdc9548c2b8e80c7b
Chris Morris <cmorris@ccistudios.com>
2015-05-13 14:12:39 -0400

    Fix the features missing carousel functionality after changing the category

--- sites/all/themes/otools/js/features.js
+++ sites/all/themes/otools/js/features.js
@@ -1,10 +1,24 @@
 (function($){
     $(function(){
+        initCarousel();
+        $(document).ajaxComplete(ajaxComplete);
+    });
+    
+    function ajaxComplete(e, xhr, settings)
+    {
+        if (settings.url == Drupal.settings.basePath + "?q=views/ajax" || settings.url == Drupal.settings.basePath + "views/ajax" || settings.url == Drupal.settings.basePath + "?q=system/ajax" ||  settings.url == Drupal.settings.basePath + "system/ajax")
+        {
+            initCarousel();
+        }
+    }
+    
+    function initCarousel()
+    {
         $(".view-features .view-content").slick({
             slide: '.views-row',
             dots: false,
             arrows: true,
             slidesToShow: 3
         });
-    });
+    }
 })(jQuery);
