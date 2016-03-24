a5ec6715ad5cba465550add8b0cd8c5d7f348727
Chris Morris <cmorris@ccistudios.com>
2016-03-24 12:42:04 -0400

    style the parts section like the rentals

---
+++ sites/all/themes/otools/templates/node--parts.tpl.php
@@ -0,0 +1,12 @@
+<?php
+print views_embed_view('rental_detail', 'block', $node->nid);
+
+$tids = array();
+foreach ($node->field_parts_category['und'] as $term)
+{
+    $tids[] = $term['tid'];
+}
+print '<div class="related-items-container"><h2>Related Items</h2>';
+print views_embed_view('related_items', 'block', implode(',',$tids));
+print '</div>';
+?>
