fc9b647d015c9a4a3e1288174f6be81882f59701
Chris Morris <cmorris@ccistudios.com>
2016-03-28 10:26:10 -0400

    fix layout of about page for < tablet sizes

---
+++ sites/all/themes/otools/js/minheight.js
@@ -0,0 +1,20 @@
+(function($){
+    var $pageWrapper;
+    var $mainWrapper;
+    
+    $(function(){
+        $pageWrapper = $(".page-wrapper");
+        $mainWrapper = $(".main-wrapper");
+        
+        resize();
+        $(window).resize(resize);
+    });
+    
+    function resize() {
+        if ($pageWrapper.height() < $(window).height()) {
+            var diff = $(window).height() - $pageWrapper.height();
+            $mainWrapper.css("min-height", $mainWrapper.height() + diff);
+        }
+    }
+    
+})(jQuery);
