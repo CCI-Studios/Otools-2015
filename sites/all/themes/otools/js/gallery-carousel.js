b276d4e3a47dc78aeaabf146cb414d3b16b33909
Chris Morris <cmorris@ccistudios.com>
2017-01-26 13:50:38 -0500

    sliders for photo galleries close #39 close #38

---
+++ sites/all/themes/otools/js/gallery-carousel.js
@@ -0,0 +1,9 @@
+(function($){
+    $(function(){
+        $(".node-event-photos .field-name-field-event-photos .field-items, .node-golf-cart-gallery .field-items").slick({
+            dots: false,
+            arrows: true,
+            adaptiveHeight: true,
+        });
+    });
+})(jQuery);
