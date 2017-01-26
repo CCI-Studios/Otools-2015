fcd63d3c10f6b402424a894017a8faa57da5260f
Chris Morris <cmorris@ccistudios.com>
2017-01-26 14:26:09 -0500

    undo the adaptive height for photo galleries

--- sites/all/themes/otools/js/gallery-carousel.js
+++ sites/all/themes/otools/js/gallery-carousel.js
@@ -3,7 +3,6 @@
         $(".node-event-photos .field-name-field-event-photos .field-items, .node-golf-cart-gallery .field-items").slick({
             dots: false,
             arrows: true,
-            adaptiveHeight: true,
         });
     });
 })(jQuery);
