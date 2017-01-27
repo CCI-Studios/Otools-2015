ae90c51455d46fa77f58565050fa342ff331254c
Chris Morris <cmorris@ccistudios.com>
2017-01-27 13:22:46 -0500

    search form popup close #49

---
+++ sites/all/themes/otools/js/search.js
@@ -0,0 +1,16 @@
+(function($){
+    $(function(){
+        $("#block-menu-menu-top-menu .search a").click(function(e){
+          if ($(window).width() <= 750) {
+            return;
+          }
+          e.preventDefault();
+          var $searchform = $(".navigation .search-form")
+          $searchform.slideToggle(250, function(){
+            if ($searchform.css("display") == "block") {
+              $searchform.find("input[type=text]").focus();
+            }
+          });
+        });
+    });
+})(jQuery);
