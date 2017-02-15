97e16dd3a676ea2343939beb7fd1d41bc62d26cd
Chris Morris <cmorris@ccistudios.com>
2017-02-15 12:20:32 -0500

    drush alias for live site

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -10,3 +10,14 @@ $aliases['dev'] = array(
 	),
 	'ssh-options'=> '-p 37241'
 );
+
+$aliases['live'] = array(
+	'uri'=> 'www.otools.ca',
+	'root' => '/home/otools/subdomains/live/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'otools',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+	'ssh-options'=> '-p 37241'
+);
