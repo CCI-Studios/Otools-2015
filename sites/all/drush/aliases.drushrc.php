c51abfee294c63ffd52880a27954693c4f30790d
Chris Morris <cmorris@ccistudios.com>
2020-05-13 11:58:00 -0400

    updated drush alias and fixed htaccess to work in a dev env

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -12,11 +12,11 @@ $aliases['dev'] = array(
 
 $aliases['live'] = array(
 	'uri'=> 'www.otools.ca',
-	'root' => '/home/otools/subdomains/live/public_html',
+	'root' => '/home/otools/public_html',
 	'remote-host'=> 'host.ccistudios.com',
 	'remote-user'=> 'otools',
 	'path-aliases'=> array(
 		'%files'=> 'sites/default/files',
-		'%drush-script'=> '/home/otools/.composer/vendor/bin/drush'
+		'%drush-script'=> '/home/otools/.config/composer/vendor/bin/drush'
 	),
 );
